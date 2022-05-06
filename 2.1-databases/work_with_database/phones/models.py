f35a53068e537046a05e47f3e3ef19941d599914
IPro-j <91323030+IPro-j@users.noreply.github.com>
2022-05-06 19:53:17 +0700

    Update models.py

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.indexes
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)
+    image = models.TextField(max_length=500)
+    release_date = models.CharField(max_length=10)
+    lte_exists = models.BooleanField(default=0)
+    slug = models.CharField(max_length=100)    
