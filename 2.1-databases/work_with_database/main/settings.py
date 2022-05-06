3a724df03df993debf536e0e2cc406b0d18152fc
IPro-j <91323030+IPro-j@users.noreply.github.com>
2022-05-06 19:49:16 +0700

    Update settings.py

--- 2.1-databases/work_with_database/main/settings.py
+++ 2.1-databases/work_with_database/main/settings.py
@@ -50,10 +50,10 @@ MIDDLEWARE = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'netology_import_phones',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        #'HOST': '127.0.0.1',
+        #'PORT': '5432',
     }
 }
 
