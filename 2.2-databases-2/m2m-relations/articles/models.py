c5eff5d7064ade0d195008d922cc46dbba8199bf
IPro-j <91323030+IPro-j@users.noreply.github.com>
2022-05-13 20:36:36 +0700

    Update models.py

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -14,3 +14,27 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Тематика')
+    articles = models.ManyToManyField(Article, related_name='subjects', through='ArticleSubjectRelation')
+
+    class Meta:
+        verbose_name = 'Разделы'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleSubjectRelation(models.Model):
+    tag = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='scopes', verbose_name='Тематика')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes', verbose_name='Статья')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематика статьи'
+        ordering = ['-is_main']
+        unique_together = [['article', 'tag']]
