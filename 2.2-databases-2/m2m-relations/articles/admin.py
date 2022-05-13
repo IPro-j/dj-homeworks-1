8ea8759149184a043493f5001ba4fceb7b607f97
IPro-j <91323030+IPro-j@users.noreply.github.com>
2022-05-13 20:35:49 +0700

    Update admin.py

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, ArticleSubjectRelation, Subject
 
-from .models import Article
+
+class ArticleRelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_cnt = 0
+        for form in self.forms:
+            print(form.cleaned_data)
+            main_cnt = main_cnt + form.cleaned_data.get('is_main', False)
+            if main_cnt > 1:
+                raise ValidationError('Основным может быть только один раздел')
+        if not main_cnt:
+            raise ValidationError('Укажите основной раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ArticleRelationshipInline(admin.TabularInline):
+    model = ArticleSubjectRelation
+    formset = ArticleRelationshipInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ArticleRelationshipInline]
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
     pass
