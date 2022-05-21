a54876bbbccf48b19415f26d2c06dd9c07364fe7
IPro-j <91323030+IPro-j@users.noreply.github.com>
2022-05-21 13:27:13 +0700

    Update models.py

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -7,7 +7,7 @@ class Sensor(models.Model):
 
 
 class Measurement(models.Model):
-    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    sensor = models.ForeignKey(Sensor, related_name='measurements', on_delete=models.CASCADE)
     temperature = models.DecimalField(max_digits=3, decimal_places=1)
     created_at = models.DateTimeField(auto_now_add=True)
 
