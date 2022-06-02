aaeedc2d99efa6beb339ede276c1aba03da620b9
IPro-j <91323030+IPro-j@users.noreply.github.com>
2022-06-02 12:50:15 +0700

    Update views.py

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,15 +1,27 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+
+from advertisements.serializers import AdvertisementSerializer
+from advertisements.models import Advertisement
+from advertisements.filters import AdvertisementFilter
+from advertisements.permissions import IsOwner
 
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend, ]
+    filter_class = AdvertisementFilter
 
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["delete", "update", "partial_update"]:
+            return [IsAuthenticated(), IsOwner()]
+        elif self.action in ["create"]:
             return [IsAuthenticated()]
         return []
