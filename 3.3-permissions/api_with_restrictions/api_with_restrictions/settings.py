ca34d943b8b521b7922787edd9f90b41d647a80e
IPro-j <91323030+IPro-j@users.noreply.github.com>
2022-06-02 12:53:45 +0700

    Update settings.py

--- 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
@@ -76,6 +76,17 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '20/minute',
+        'anon': '10/minute',
+    },
+    'DEFAULT_FILTER_BACKLENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
     ]
 }
 
@@ -87,10 +98,10 @@ WSGI_APPLICATION = 'api_with_restrictions.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'netology_classified_ads',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        #'HOST': '127.0.0.1',
+        #'PORT': '5432',
     }
 }
 
