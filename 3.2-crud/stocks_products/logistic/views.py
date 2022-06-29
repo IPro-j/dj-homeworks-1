c76b578a77411d8495f442a9eabb5400be855658
IPro-j <91323030+IPro-j@users.noreply.github.com>
2022-06-29 17:34:51 +0700

    Update views.py

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,3 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -7,12 +10,17 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    #filter_backends = ['title', ]
-    #search_field = ['title', ]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title', 'description', ]
+    ordering_fields = ['id', 'title', ]
+    pagination_class = LimitOffsetPagination
     # при необходимости добавьте параметры фильтрации
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['products', ]
+    pagination_class = LimitOffsetPagination
     # при необходимости добавьте параметры фильтрации
